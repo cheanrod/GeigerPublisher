30009468c8117982208346839cf8ec0c5378000f
Sven Werner <privat@svenwerner.de>
2020-01-19 21:38:40 +0100

    use args in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
+ARG DOTNET_RUNTIME=linux-x64
+ARG IMAGE_TAG=3.1-buster-slim
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+ARG DOTNET_RUNTIME=linux-x64
+
 WORKDIR /app
 
 # copy csproj and restore as distinct layers
@@ -10,10 +14,10 @@ RUN dotnet restore
 # copy everything else and build app
 COPY GeigerPublisher/. ./GeigerPublisher/
 WORKDIR /app/GeigerPublisher
-RUN dotnet publish -c Release -o out
+RUN dotnet publish -r $DOTNET_RUNTIME -c Release -o out
 
 
-FROM mcr.microsoft.com/dotnet/core/runtime:3.1-buster-slim AS runtime
+FROM mcr.microsoft.com/dotnet/core/runtime:$IMAGE_TAG AS runtime
 WORKDIR /app
 COPY --from=build /app/GeigerPublisher/out ./
 ENTRYPOINT ["dotnet", "GeigerPublisher.dll"]
--- scripts/docker-multiarch-push.sh
+++ scripts/docker-multiarch-push.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 docker build -t cheanrod/geigerpublisher:amd64 -f Dockerfile .
-docker build -t cheanrod/geigerpublisher:arm32v7 -f Dockerfile.arm32v7 .
-docker build -t cheanrod/geigerpublisher:arm64v8 -f Dockerfile.arm64v8 .
+docker build -t cheanrod/geigerpublisher:arm32v7 --build-arg DOTNET_RUNTIME=linux-arm \
+ --build-arg IMAGE_TAG=3.1-buster-slim-arm32v7 -f Dockerfile .
+docker build -t cheanrod/geigerpublisher:arm64v8 --build-arg DOTNET_RUNTIME=linux-arm64 \
+ --build-arg IMAGE_TAG=3.1-buster-slim-arm64v8 -f Dockerfile .
 
 docker push cheanrod/geigerpublisher:amd64
 docker push cheanrod/geigerpublisher:arm32v7
 docker push cheanrod/geigerpublisher:arm64v8
 
-docker manifest create cheanrod/geigerpublisher:latest cheanrod/geigerpublisher:amd64 cheanrod/geigerpublisher:arm32v7 cheanrod/geigerpublisher:arm64v8
+docker manifest create --amend cheanrod/geigerpublisher:latest cheanrod/geigerpublisher:amd64 cheanrod/geigerpublisher:arm32v7 cheanrod/geigerpublisher:arm64v8
 
 docker manifest annotate cheanrod/geigerpublisher:latest cheanrod/geigerpublisher:arm32v7 --os linux --arch arm --variant v7
 docker manifest annotate cheanrod/geigerpublisher:latest cheanrod/geigerpublisher:arm64v8 --os linux --arch arm64 --variant v8
 
 docker manifest inspect cheanrod/geigerpublisher:latest
 
-docker manifest push cheanrod/geigerpublisher:latest
+docker manifest push --purge cheanrod/geigerpublisher:latest
